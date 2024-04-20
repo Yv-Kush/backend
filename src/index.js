0222f2e79006bd1a52d07c79e21506e690f53990
yv_kush <yvkush@gmail.com>
2024-04-20 18:37:26 +0530

    createting app from express()

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import dotenv from "dotenv"
-
+import app from './app.js'
 import connectDB from "./db/index.js"
 
 connectDB()
+.then( ()=> {
+    app.listen(process.env.PORT || 8000, ()=> {
+        console.log("server is listing on port ", process.env.PORT)
+    })
+})
+.catch( (error)=> {
+    console.log("error on connecting Database", error)
+})
+
+
 
 dotenv.config({
     path: "./env"
